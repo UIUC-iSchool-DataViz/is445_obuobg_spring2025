e2b727bd783537a74cd58c77463c0d69a0a3af3e
jnaiman <jnaiman.illinois.edu>
2025-04-02 18:43:51 -0500

    Auto-commit: Wed Apr  2 18:43:51 CDT 2025

--- week11/inClass_script_week10.py
+++ week11/inClass_script_week10.py
@@ -11,3 +11,7 @@ st.subheader('Images')
 st.image('https://i.redd.it/on-a-scale-of-1-10-how-derpy-is-she-v0-z8gtdwu5n5zb1.jpg?width=3024&format=pjpg&auto=webp&s=345e7e1d5b45f20c733e497a9f746f4cbd3a61da', 
          width=200, 
          caption='A thinly veiled excuse for a derpy corgi.')
+
+import numpy as np
+img_data = np.random.random((200,200))
+st.image(img_data, caption='random data')
